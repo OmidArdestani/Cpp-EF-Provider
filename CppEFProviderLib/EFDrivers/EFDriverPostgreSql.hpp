8fcb95f4edff55078330aa2f1f58f10260d6001a
Omid91a <35274928+Omid91a@users.noreply.github.com>
2023-12-12 17:44:45 +0800

    0.9.5.1
    Implemented pure virtual for Posgtres.

--- CppEFProviderLib/EFDrivers/EFDriverPostgreSql.hpp
+++ CppEFProviderLib/EFDrivers/EFDriverPostgreSql.hpp
@@ -18,5 +18,17 @@ namespace EFProvider
 		{
 
 		}
+
+	public:
+		virtual T* SingleOrDefault(std::string condition) { return new T(); }
+		virtual T* LastItem() { return new T(); }
+		virtual T* Find(int id) { return new T(); }
+		virtual std::list<std::list<std::string>> Select(std::string property_name, std::string condition) { return std::list<std::list<std::string>>(); }
+		virtual std::list<T*> Where(std::string condition) { return std::list<T*>(); }
+		virtual std::list<T*> ToList() { return std::list<T*>(); }
+		virtual std::list<T*> Top(int number) { return std::list<T*>(); }
+		virtual std::list<T*> GetPagedData(int pageIndex, int number_of_item_per_page, std::string orderByProperty) { return std::list<T*>(); }
+		virtual int Count() { return -1; }
+		virtual int GetMaxID() { return -1; }
 	};
 }
